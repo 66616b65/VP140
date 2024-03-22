4f2e57e636c9fad3ba4d51fec99068737e5be5d8
Natalia Khizrieva <jesusstern@gmail.com>
2024-03-22 14:44:52 +0300

    linq demo

---
+++ Demos/VPExamples/LINQExample/LinqExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExample
+{
+    public static class LinqExample
+    {
+        public static void Demo()
+        {
+            var intCollection = new List<int>() { 1, 6, 8, 23, -2 };
+            var hasNegative = intCollection.Any(n => n < 0);
+            var sort = intCollection.OrderByDescending(n => n);
+            var where = intCollection.Where(x => x > 10 || (x / 2) > 20).OrderBy(x => x).GetEnumerator();
+
+
+
+            var userCollection = new List<User>();
+            userCollection.Add(new User(1, "Kiska"));
+            userCollection.Add(new User(2, "Krasotka"));
+            userCollection.Add(new User(15, "MegaProger"));
+
+            var atIndexUser = userCollection.ElementAt(19);
+            var whereUser = userCollection.Where(x => x.Name.Length > 5).ToList();
+            var skipUser = userCollection.Skip(2);
+
+            var myConcatREsult = userCollection.MyConcatMethod(whereUser).Except(skipUser).ToList();
+        }
+
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LastVisit { get; set; }
+
+        public User (int id, string name)
+        {
+            Id = id;
+            Name = name;
+            LastVisit = DateTime.Now;
+        }
+
+        public void Visit(DateTime dateTime)
+        {
+            LastVisit = dateTime;
+        }
+
+    }
+}
---
+++ Demos/VPExamples/LINQExample/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQExample
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TSource> MyConcatMethod<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            var result = new List<TSource>();
+
+            result.AddRange(first);
+            result.AddRange(second);
+
+            return result;
+        }
+    }
+}
